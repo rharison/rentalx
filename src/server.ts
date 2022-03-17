e23a28ed09d0f77e854b46270649607e7910ef19
rharison <rharison.abreu@gmail.com>
2022-03-17 19:17:36 -0300

    Adicionando função de cadastro de categorias

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
-const app = express();
+import { categoriesRoutes } from './routes/categories.routes';
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+const app = express();
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running!'));
