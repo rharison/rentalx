04cb9819ab87be127343a05125317e2212654969
rharison <rharison.abreu@gmail.com>
2022-03-18 16:09:42 -0300

    Adicionando funcao de upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
